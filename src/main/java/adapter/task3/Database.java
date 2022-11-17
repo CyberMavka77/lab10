5b51696d67dcaffc16d00def65df8a0dc816e13d
cybermavka <svettacifra@gmail.com>
2022-11-17 18:22:23 +0200

    version 1

---
+++ src/main/java/adapter/task3/Database.java
@@ -0,0 +1,11 @@
+package adapter.task3;
+
+public class Database extends БазаДаних {
+    public String retrieveUserData() {
+        return отриматиДаніКористувача();
+    }
+
+    public String retrieveStatic() {
+        return отриматиСтатистичніДані();
+    }
+}
